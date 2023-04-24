8f4df13ce5ef4311d04cf80b480c4effd7f7db84
tai2511 <nguyentai2511myv@gmail.com>
2023-04-24 16:07:24 +0700

    Revert "responsive project page"
    This reverts commit 99223934

--- src/resources/views/clients/projects/projects_list.blade.php
+++ src/resources/views/clients/projects/projects_list.blade.php
@@ -19,57 +19,46 @@
             <button class="filter_btn">name</button>
             <button class="filter_btn">active</button>
         </div>
-        @if (count($projects) == 0)
-        <p>No data available...</p>
-        @else
-        @php
-            $numberTable = 20;
-            $widthProjectTable = 100 / ($numberTable/10);
-            $widthProjectTable.= '%';
-        @endphp
-        <div class="d-flex">
-            @for($i = 0; $i < $numberTable/10; $i++)
-                <div class="project-table" style="width: {{ $widthProjectTable }}">
-                    <div class="tbl">
-                        <div class="tr">
-                            <div class="td th" style="padding-left: 38px; width: 20%;">star</div>
-                            <div class="td th">name</div>
-                            <div class="td th" style="width: 12%;">active</div>
-                            <div class="td" style="width: 8%;"></div>
-                        </div>
-                    </div>
-                    <div class="container_tbl">
-                        <div class="tbl" style="margin-top: 0;">
-                            @for ($j = $i * 10 ; $j < $i * 10  + 10 ; $j++)
-                                @php
-                                    if (!isset($projects[$j])) break;
-                                @endphp
-                                <div class="tr2">
-                                    <div class="td d-flex" style="padding-left: 38px; width: 20%;">
-                                        <input class="star" type="checkbox" title="bookmark page" checked=""><br>
-                                    </div>
-                                    <div class="td">
-                                        <div class="name_box"> IT </div>
-                                        <div class="name list_link"> {{ $projects[$j]->project_name }} </div>
-                                    </div>
-                                    <div class="td" style="width: 12%;">
-                                        <label class="switch mb-0">
-                                            <input type="checkbox" onclick="return false;" {{ $projects[$j]->active ? "checked" : ""}}>
-                                            <span class="slider round"></span>
-                                        </label>
-                                    </div>
-                                    <div class="td" style=" width: 8%;">
-                                        <a href="{{ route('project.edit', $projects[$j]->project_id) }}"><i class="project-edit-dot">...</i></a>
-                                    </div>
-                                </div>
-                            @endfor
-                        </div>
-                    </div>
-                </div>
-            @endfor
+        <div class="tbl">
+            <div class="tr">
+                <div class="td th" style="padding-left: 38px; width: 20%;">star</div>
+                <div class="td th" style="width: 20%;">name</div>
+                <div class="td th" style="width: 20%;">client</div>
+                <div class="td th" style="width: 20%;">lead</div>
+                <div class="td th" style="width: 12%;">active</div>
+                <div class="td" style="width: 8%;"></div>
+            </div>
         </div>
+        @if (!empty($projects))
+            <div class="container_tbl">
+                <div class="tbl" style="margin-top: 0;">
+                    @foreach ($projects as $project)
+                        <div class="tr2">
+                            <div class="td d-flex" style="padding-left: 38px; width: 20%;">
+                                <input class="star" type="checkbox" title="bookmark page" checked=""><br>
+                            </div>
+                            <div class="td" style="width: 20%;">
+                                <div class="name_box"> IT </div>
+                                <div class="name list_link"> {{ $project->project_name }} </div>
+                            </div>
+                            <div class="td" style="width: 20%;"> {{ $project->client_name }} </div>
+                            <div class="td" style="width: 20%;"> {{ $project->lead_name }} </div>
+                            <div class="td" style="width: 12%;">
+                                <label class="switch mb-0">
+                                    <input type="checkbox" onclick="return false;" {{ $project->active ? "checked" : ""}}>
+                                    <span class="slider round"></span>
+                                </label>
+                            </div>
+                            <div class="td" style=" width: 8%;">
+                                <a href="{{ route('project.edit', $project->project_id) }}"><i class="project-edit-dot">...</i></a>
+                            </div>
+                        </div>
+                    @endforeach
+                </div>
+            </div>
+        @else
+            <p>No data available...</p>
         @endif
-
     </div>
     <div class="area" id="settings_tab"></div>
 @endsection
