9044f77ea5085bc64d818105a33b90783c0ee12a
tai2511 <nguyentai2511myv@gmail.com>
2023-04-24 16:12:06 +0700

    Revert "asdad"
    This reverts commit 66f3c4e9

--- src/resources/views/clients/projects/create.blade.php
+++ src/resources/views/clients/projects/create.blade.php
@@ -3,37 +3,34 @@
     @parent
     <div class="area project_add_new" id="project_tab">
         <p class="title">Project</p>
-        <h2 class="d-flex mb-4 mt-4 position-relative new-project">
-            New Projects
-            <div class="upload_container" style="background-image: url('{{ asset('assets/images/project/pattern.svg') }}')" onclick="document.getElementById('upload_file').click();">
-                <input type="file" id="upload_file" style="display: none;">
-                <div class="upload_btn">Upload</div>
+        <h2 class="d-flex position-relative new-project">New Projects</h2>
+        <div class="upload_container" style="background-image: url('{{ asset('assets/images/project/pattern.svg') }}')" onclick="document.getElementById('upload_file').click();">
+            <input type="file" id="upload_file" style="display: none;">
+            <div class="upload_btn">Upload</div>
+        </div>
+        <div>
+            <div class="d-flex group-input">
+                <p class="input_caption">Name<sup>*</sup></p>
+                <input placeholder="Try a team name, project goal, milestone...">
             </div>
-        </h2>
-        <div style="width: 85%;">
-            <div class="d-flex group-input row">
-                <p class="col-md-2 input_caption mb-1">Name</p>
-                <input class="form-control col-md-10" placeholder="Try a team name, project goal, milestone...">
+            <div class="d-flex group-input">
+                <p class="input_caption">Key<sup>*</sup></p>
+                <input placeholder="Project Key is Unique">
             </div>
-            <div class="d-flex group-input row">
-                <p class="col-md-2 input_caption mb-1">Key</p>
-                <input class="form-control col-md-10" placeholder="Project Key is Unique">
+            <div class="d-flex group-input">
+                <p class="input_caption">Lead<sup>*</sup></p>
+                <input placeholder="Default Your Account">
             </div>
-            <div class="d-flex group-input row">
-                <p class="col-md-2 input_caption mb-1">Lead</p>
-                <input class="form-control col-md-10" placeholder="Default Your Account">
+            <div class="d-flex group-input">
+                <p class="input_caption">Description</p>
+                <input placeholder="Default Your Account">
             </div>
-            <div class="d-flex mb-3 row">
-                <p class=" col-md-2 input_caption mb-1">Description</p>
-                <textarea class="form-control col-md-10" rows="5">
-            </textarea>
-            </div>
-            <div class="flex float-right">
+            <div class="flex float-right action-create">
                 <a class="text-white" href="{{ route('project.index') }}">
                     <button class="btn pt-0 pb-0 pr-5 pl-5 mr-3">Cancel</button>
                 </a>
                 <a class="text-white">
-                    <button class="btn pt-0 pb-0 pr-5 pl-5 mr-3">Save</button>
+                    <button class="btn pt-0 pb-0 pr-5 pl-5">Save</button>
                 </a>
             </div>
         </div>
